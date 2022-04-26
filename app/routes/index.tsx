a2bc15257a36c964b22a6b5582c3d31d1a354fb2
Jonas Glerup Røssum <1959615+joglr@users.noreply.github.com>
2022-04-26 21:16:00 +0200

    Add remix app with scooters

---
+++ app/routes/index.tsx
@@ -0,0 +1,59 @@
+import { useState } from "react"
+import qr1 from "~/qr/1.png"
+import qr2 from "~/qr/2.png"
+import qr3 from "~/qr/3.png"
+import qr4 from "~/qr/4.png"
+import qr5 from "~/qr/5.png"
+import qr6 from "~/qr/6.png"
+
+const scooters = [
+  { qrSrc: qr1 },
+  { qrSrc: qr2 },
+  { qrSrc: qr3 },
+  { qrSrc: qr4 },
+  { qrSrc: qr5 },
+  { qrSrc: qr6 },
+]
+
+export default function Index() {
+  const [currentScooter, setCurrentScooter] = useState(0)
+  return (
+    <div
+      style={{
+        fontFamily: "system-ui, sans-serif",
+        lineHeight: "1.4",
+        maxWidth: "600px",
+        margin: "0 auto",
+      }}
+    >
+      <h1>Scooter booking</h1>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(2, 1fr)",
+          placeItems: "center",
+        }}
+      >
+        <button
+          onClick={() => setCurrentScooter(currentScooter - 1)}
+          disabled={currentScooter === 0}
+        >
+          Previous scooter
+        </button>
+        <button
+          onClick={() => setCurrentScooter(currentScooter + 1)}
+          disabled={currentScooter === scooters.length - 1}
+        >
+          Next scooter
+        </button>
+      </div>
+      <h2>
+        Scooter {currentScooter + 1} of {scooters.length}
+      </h2>
+      <img
+        src={scooters[currentScooter].qrSrc}
+        alt={`Scooter ${currentScooter + 1}`}
+      />
+    </div>
+  )
+}
